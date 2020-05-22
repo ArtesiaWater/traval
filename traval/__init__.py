be95d75b6efef51bbaeacb329370292befde6f46
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2020-05-22 13:26:26 +0200

    add package init

---
+++ traval/__init__.py
@@ -0,0 +1,5 @@
+from . import rulelib, ts_utils, plots
+from .detector import Detector
+from .plots import ComparisonPlots
+from .ruleset import RuleSet
+from .ts_comparison import SeriesComparison, SeriesComparisonRelative
