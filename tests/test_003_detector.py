c956279f27a340d8da2c51b5d728f1b5635c3cc1
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2020-05-22 13:30:05 +0200

    add detector tests

---
+++ tests/test_003_detector.py
@@ -0,0 +1,42 @@
+import traval
+import pandas as pd
+import numpy as np
+
+from test_002_ruleset import test_add_rules
+
+
+def test_init_detector():
+    s = pd.Series(index=range(10),
+                  data=np.arange(-5, 23, 3, dtype=np.float),
+                  name="test_series")
+    d = traval.Detector(s)
+    return d
+
+
+def test_repr():
+    d = test_init_detector()
+    return d.__repr__
+
+
+def test_add_truth():
+    t = pd.Series(index=range(10),
+                  data=np.arange(-5, 23, 3, dtype=np.float),
+                  name="test_series")
+    t[t < 0] = np.nan
+    t[t > 10] = np.nan
+    d = test_init_detector()
+    d.set_truth(t)
+    return d
+
+
+def test_apply_ruleset():
+    rset = test_add_rules()
+    d = test_add_truth()
+    d.apply_ruleset(rset)
+    return d
+
+def test_reset():
+    d = test_apply_ruleset()
+    d.reset()
+    assert not hasattr(d, "ts_result")
+    return
