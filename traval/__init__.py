3dd2396fad97cee1ffd483a7ef733ca250c12cbe
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2020-05-26 16:41:18 +0200

    update init

--- traval/__init__.py
+++ traval/__init__.py
@@ -2,4 +2,5 @@ from . import rulelib, ts_utils, plots
 from .detector import Detector
 from .plots import ComparisonPlots
 from .ruleset import RuleSet
+from .binary_classifier import BinaryClassifier
 from .ts_comparison import SeriesComparison, SeriesComparisonRelative
