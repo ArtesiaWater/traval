8ad90ac3c29cd6f1289cfd2549620b8b15dbc1e7
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2021-06-16 11:13:00 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
-from setuptools import setup, find_packages
+from os import path
 
-try:
-    import pypandoc
-    l_d = pypandoc.convert('README.md')
-except:
-    l_d = ''
+from setuptools import find_packages, setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    l_d = f.read()
 
 # Get the version.
 version = {}
-with open("version.py") as fp:
+with open("traval/version.py") as fp:
     exec(fp.read(), version)
 
 setup(
     name='traval',
     version=version['__version__'],
-    description='traval module by Artesia',
+    description='Python package for error detection in time series. '
+                'Create custom error detection algorithms to support '
+                ' in data validation.',
     long_description=l_d,
+    long_description_content_type='text/markdown',
     url='https://github.com/ArtesiaWater/traval',
     author='Artesia',
     license='MIT',
@@ -24,9 +27,14 @@ setup(
         'Intended Audience :: Science/Research',
         'Intended Audience :: Other Audience',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Scientific/Engineering :: Hydrology',
     ],
     platforms='Windows, Mac OS-X, *nix',
-    install_requires=['numpy>=1.18', 'matplotlib>=3.0', 'pandas>=0.25', ],
+    install_requires=['numpy>=1.18',
+                      'matplotlib>=3.0',
+                      'pandas>=0.25'],
     packages=find_packages(exclude=[])
 )
